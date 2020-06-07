1e781794b79071d9357874c770988ff28cd86f6f
Sergei M <black_white2018@rambler.ru>
2020-06-07 22:37:43 +0300

    Updates

---
+++ precache-manifest.ef4e75ea2a0ebf59717373541164e3c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5caa51c3c8c6a9c86d9bd09a529002c2",
+    "url": "/listing-product-from-json/index.html"
+  },
+  {
+    "revision": "95e11c8adb46cd1fa241",
+    "url": "/listing-product-from-json/static/css/main.4f58aad1.chunk.css"
+  },
+  {
+    "revision": "95dbbde9dfdbfacc324d",
+    "url": "/listing-product-from-json/static/js/2.e2d01dc9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/listing-product-from-json/static/js/2.e2d01dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e11c8adb46cd1fa241",
+    "url": "/listing-product-from-json/static/js/main.d42ba612.chunk.js"
+  },
+  {
+    "revision": "1838e3b95ccfb9bcf6c8",
+    "url": "/listing-product-from-json/static/js/runtime-main.b10c1ef6.js"
+  }
+]);
